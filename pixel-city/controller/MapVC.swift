45a3a257f92eda45933a54b5fad6ab42e40ca6ba
FranSalom <fransalomt@gmail.com>
2017-11-06 11:59:59 -0400

    Creacion del Proyecto

---
+++ pixel-city/controller/MapVC.swift
@@ -0,0 +1,25 @@
+//
+//  MapVC.swift
+//  pixel-city
+//
+//  Created by Francisco Salom on 3/11/17.
+//  Copyright © 2017 Francisco Salom. All rights reserved.
+//
+
+import UIKit
+
+class MapVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
